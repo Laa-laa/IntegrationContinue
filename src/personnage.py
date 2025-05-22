6313eced7ab9da3c427d9abbec34a59b1471d0c3
Lala <kozanlelia@hotmail.com>
2025-05-22 12:03:20 +0200

    ajout feature (echec)

--- src/personnage.py
+++ src/personnage.py
@@ -4,3 +4,7 @@ class Personnage:
 
     def est_vivant(self):
         return self.hp > 0
+    
+    def recevoir_attaque(self):
+        if self.est_vivant():
+            self.hp -= 2 #faux, essai echec test
